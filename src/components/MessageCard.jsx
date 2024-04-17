5ac9b597511a23938bdf7dfd45bbe17533e4eef9
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-17 09:10:29 +0000

    edited the artisan dashboard

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function MessageCard(props) {
     return (
-        <div class="flex flex-col h-screen max-w-sm">
+        <div class="flex flex-col h-screen max-w-sm border border-gray-200 rounded-lg mx-40 mt-10 px-4">
     <div class="flex-grow overflow-y-auto">
         {/* <!-- Chat messages --> */}
         <div class="flex flex-col mb-4 gap-4 py-4">
@@ -12,7 +12,7 @@ function MessageCard(props) {
                 </div>
             </div>
             <div class="flex justify-end">
-                <div class="bg-blue-500 rounded-lg px-4 py-2 max-w-[80%]">
+                <div class="bg-purple-500 rounded-lg px-4 py-2 max-w-[80%]">
                     <p class="text-white text-sm">I'm good, thanks! How about you?</p>
                 </div>
             </div>
@@ -26,7 +26,7 @@ function MessageCard(props) {
     <div class="flex justify-center items-center h-16">
         {/* <!-- Chat input --> */}
         <input type="text" class="border border-gray-300 rounded-lg py-2 px-4 w-full max-w-lg mr-4" placeholder="Type a message..."/>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Send</button>
+        <button class="bg-purple-900 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Send</button>
     </div>
 </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-    <header className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-purple-600 bg-opacity-50 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
+    <header className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-black bg-opacity-50 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
         <div className="px-4">
           <div className="flex items-center justify-between">
             <div className="flex shrink-0">
