9f6693286b5b9460685b80b4b8c7db823ac96575
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-17 23:27:10 +0000

    minor changes

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -5,7 +5,7 @@ function Process() {
     <div>
       <section className="relative overflow-hidden bg-purple-200 dark:bg-gray-900">
         <div className="mt-2 md:mt-0 py-4 pb-4 sm:py-16 lg:pb-24 overflow-hidden">
-        <h2 className="text-center text-purple-400 font-bold text-2xl">Our Process</h2>
+        <h2 id="process" className="text-center text-purple-400 font-bold text-2xl">Our Process</h2>
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 relative">
             
             <div className="relative mt-8 lg:mt-8">
@@ -22,9 +22,9 @@ function Process() {
                   <path
                     d="M2 29C20.2154 33.6961 38.9915 35.1324 57.6111 37.5555C80.2065 40.496 102.791 43.3231 125.556 44.5555C163.184 46.5927 201.26 45 238.944 45C312.75 45 385.368 30.7371 458.278 20.6666C495.231 15.5627 532.399 11.6429 569.278 6.11109C589.515 3.07551 609.767 2.09927 630.222 1.99998C655.606 1.87676 681.208 1.11809 706.556 2.44442C739.552 4.17096 772.539 6.75565 805.222 11.5C828 14.8064 850.34 20.2233 873 24"
                     stroke="#BAA5FF"
-                    stroke-width="3"
-                    stroke-linecap="round"
-                    stroke-dasharray="1 12"
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeDasharray="1 12"
                   />
                 </svg>
               </div>
