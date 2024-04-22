cd2026077b3837a7d520c01cb702f32a0da41154
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-22 12:09:18 +0000

    mapped rating cards

--- src/components/RatingCard.jsx
+++ src/components/RatingCard.jsx
@@ -1,94 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 function RatingCard(props) {
+
+const [ratingList, setRatingList] =useState([]);
+
+useEffect(()=> {
+  const getRatings = async () =>{
+    try{
+      const response = await axios.get(
+        `${process.env.REACT_APP_ARTISCHED_API}/get-ratings`
+      );
+      setRatingList(response.data);
+    } catch(error){
+      console.error("Error fetching ratings:", error)
+    }
+  };
+  getRatings();
+}, []);
+
   return (
     <div>
 <h2 id="review" className="py-4 text-center text-orange-400 font-bold text-2xl">Client Reviews</h2>
-
-      <div className="flex flex-col justify-between rounded-md border border-neutral-800 bg-black p-4 shadow-sm max-w-sm mx-auto my-8">
+{ratingList.map((ratings)=>(
+      <div 
+      key={ratings.id} 
+      className="flex flex-col justify-between rounded-md border border-neutral-800 bg-black p-4 shadow-sm max-w-sm mx-auto my-8">
         {/* <!-- stars --> */}
-        <div className="text-violet-500 flex gap-2">
+        <div className="text-orange-500 flex gap-2">
+          <p>{ratings.rating}/5</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="text-violet-500"
+            className="text-orange-500"
             width="20"
             height="20"
             viewBox="0 0 24 24"
             strokeWidth="2"
             stroke="currentColor"
-            fill="none"
+            fill="currentColor"
             strokeLinecap="round"
             strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
           </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-violet-500"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-violet-500"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-violet-500"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-violet-500"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
-          </svg>
+          
         </div>
 
         <p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
-          I've been using XYZ for over a year now and their customer service is
-          excellent! Whenever I have a question, the team is always available
-          and willing to help. Highly recommend!
+          {ratings.description}
         </p>
 
         <div className="mt-6 flex items-center gap-6 ">
@@ -96,7 +56,7 @@ function RatingCard(props) {
             <div className="relative inline-block overflow-hidden rounded-lg border-neutral-800">
               <img
                 alt=""
-                src="https://randomuser.me/api/portraits/women/2.jpg"
+                src={ratings.image}
                 width="50"
                 height="50"
                 decoding="async"
@@ -109,14 +69,15 @@ function RatingCard(props) {
           </div>
           <div>
             <p className="leading-relaxed tracking-wide text-gray-200">
-              Melissa Smith
+              {ratings.email}
             </p>
             <p className="text-xs leading-relaxed tracking-wide text-gray-400">
-              Marketing Manager
+              Client
             </p>
           </div>
         </div>
       </div>
+      ))}
     </div>
   );
 }
